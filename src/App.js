bd438377ff4d41b8d86c9942148073f35cf6d342
Raziel <robroblemol>
2019-03-01 14:48:36 -0500

    using click event and and setState function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+//import { Math } from 'core-js';
 
 class Status extends Component {
+  constructor(){
+    super();
+    this.state = {
+      likes:0 
+    }
+    this.like = this.like.bind(this);
+  }
+
+  like(){
+    console.log('likes');
+    this.setState({
+      likes: this.state.likes+1
+    })
+  }
+
   render(){
     return (
       <div className = "status">
-        <p>Uau!, Mi skribas en esperanto!</p>
-        <p><button>Mi sxatas gin</button></p>
+        <p>{this.props.mytext}</p>
+        <p><button onClick ={this.like}> 
+          {this.state.likes} Mi sxatas gin
+        </button></p>
       </div>
     );
   }
@@ -15,12 +33,12 @@ class Status extends Component {
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-        <Status/>
-        <Status/>
-        <Status/>
-        <Status/>
+      <div className = "App">
+        <header className = "App-header">
+        <Status mytext = "Uau! Mi skribas en esperanto!" />
+        <Status mytext = "Mi volas lerni esperanto!" />
+        <Status mytext = "Esperanto estas tre mojoso" />
+        <Status mytext = "Mi ne sicas ke skribi en ĉi tie komponanto" />
         </header>
       </div>
     );
