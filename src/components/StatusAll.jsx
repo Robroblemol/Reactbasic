e3bd156a499fc199ca6867b8638058c51316ece9
Raziel <robroblemol>
2019-03-22 16:24:47 -0500

    adding list items

--- src/components/StatusAll.jsx
+++ src/components/StatusAll.jsx
@@ -5,7 +5,7 @@ export default class StatusAll extends Component {
     constructor(props){
       super(props);
       this.state = {
-        newStatusText: 'nova tekto',   
+        newStatusText: '',   
         statuses: [
           "Uau! Mi skribas en esperanto!", 
           "Mi volas lerni esperanto!", 
@@ -14,18 +14,30 @@ export default class StatusAll extends Component {
         ]
       }
       this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
     }
   handleChange(event){
-    console.log('event.target.value',event.target.value);
+    //console.log('event.target.value',event.target.value);
     //debugger;
     this.setState({newStatusText: event.target.value});
+  }
+  handleSubmit(event){
+    //hacce que e l form no lance a una nueva pagina (o refresque)
+    //se quedara en la misma pagina 
+    event.preventDefault();
+    let newStatuses = [this.state.newStatusText, ...this.state.statuses];
+    this.setState({
+      newStatusText: '',
+      statuses: newStatuses,
+
+    });
   }
     render(){
       return (
           <div>
                 <div className = "status">
                     <h1>Alboni nova ŝtaton</h1>
-                    <form>
+                    <form onSubmit={this.handleSubmit}>
                         <input type= "text" value={this.state.newStatusText} 
                             onChange={this.handleChange}   placeholder="Kio vi volas diri?"/>
                         <button type= "submit">albonu ŝtaton</button>
